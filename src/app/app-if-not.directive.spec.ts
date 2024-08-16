da18da5b4875cf63e77f76f115ad4f64f26036cb
ashwithags <ashwithags@gmail.com>
2024-08-16 15:35:10 +0200

    If-not directive creation

--- src/app/app-if-not.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AppIfNotDirective } from './app-if-not.directive';
-
-describe('AppIfNotDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AppIfNotDirective();
-    expect(directive).toBeTruthy();
-  });
-});
