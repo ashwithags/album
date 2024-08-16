a700c2632f557b744ff81cc94d64ad982bddc118
ashwithags <ashwithags@gmail.com>
2024-08-16 15:30:29 +0200

    loged in Time calculation Pipe

--- src/app/pipe/time-ago.pipe.ts
+++ src/app/pipe/time-ago.pipe.ts
@@ -1,12 +1,33 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'timeAgo'
+  name: 'timeAgo',
+  pure: false
 })
 export class TimeAgoPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+
+    const now = new Date();
+
+    const seconds = Math.floor((now.getTime() - new Date(value).getTime()) / 1000);
+    if (seconds < 60) {
+      return `${seconds} seconds ago`;
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    if (minutes < 60) {
+      return `${minutes} minutes ago`;
+    }
+
+    const hours = Math.floor(minutes / 60);
+    if (hours < 24) {
+      return `${hours} hours ago`;
+    }
+    
+    const days = Math.floor(hours / 24);
+    return `${days} days ago`;
+
   }
 
 }
